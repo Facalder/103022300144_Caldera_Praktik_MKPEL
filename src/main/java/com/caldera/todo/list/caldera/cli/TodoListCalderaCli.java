1697d757b605833670fbd95be38e505abdc4dfbf
caldera <faainamacaldera16@gmail.com>
2026-06-08 07:27:09 +0700

    feat: init project

---
+++ src/main/java/com/caldera/todo/list/caldera/cli/TodoListCalderaCli.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.caldera.todo.list.caldera.cli;
+
+/**
+ *
+ * @author calderaly
+ */
+public class TodoListCalderaCli {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
